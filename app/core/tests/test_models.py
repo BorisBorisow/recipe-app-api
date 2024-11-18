95a47ce36d0100cfdffceefd2a8304d1a5d67178
BorisBorisowgit config --globa user.email bborrisow@gmail.com <bborrisow@gmail.com>
2024-11-18 20:32:52 +0100

    Normalize email addresses

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,19 +1,33 @@
 """
-Tests for models
+Tests for models.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-class ModelTests(TestCase):
-    """Tests for models"""
 
-    def test_create_user_with_email_successfull(self):
-        """test creating a user with an email is successful"""
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
-            password=password
+            password=password,
         )
+
         self.assertEqual(user.email, email)
-        self.assertEqual(user.check_password(password))
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
