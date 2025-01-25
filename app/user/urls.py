7d9a14dfb718a0e124165ae8179c44c807d9e4ed
BorisBorisowgit config --globa user.email bborrisow@gmail.com <bborrisow@gmail.com>
2025-01-25 18:27:03 +0100

    added serializer, CreateView und Url for the App "User"

---
+++ app/user/urls.py
@@ -0,0 +1,14 @@
+"""
+URL mapping for the user API
+"""
+
+from django.urls import path
+
+from . import views
+
+app_name = "user"
+
+urlpatterns = [
+    path('create/', views.UserCreateAPIView.as_view(), name='create'),
+    path('token/', views.TokenCreateView.as_view(), name='token'),
+]
--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+"""V
+Views for the user API
+"""
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    """View to create a new user in the system"""
+    serializer_class = UserSerializer
+
+
+class TokenCreateView(ObtainAuthToken):
+    """Create a new auth token for user."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
